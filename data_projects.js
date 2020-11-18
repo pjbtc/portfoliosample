9db88244393b3b549094ccd4e51af1cf5e2a4141
pjbtc <pjbhattachan@gmail.com>
2020-11-18 20:18:45 +0000

    Add files via upload

---
+++ data_projects.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import covid from '../assets/icons/images.jpeg';
+import devtalk from '../assets/icons/devtalk.jpeg';
+import chat from '../assets/icons/chat.jpeg';
+import health from '../assets/icons/health.jpg';
+import todo from '../assets/icons/todo.png';
+import weather from '../assets/icons/weather.jpeg';
+
+
+const data_projects=[
+		{
+			name:'covid 19 tracker',
+			image: covid,
+			deployed_url: "http://covid-19-tracker-by-pjbtc.web.app/",
+			github_url: "http://github.com/pjbtc/covit-19-tracker",
+			category:['react.js']
+		},
+		{
+			name:'Devs Talk',
+			image: devtalk,
+			deployed_url: "http://covid-19-tracker-by-pjbtc.web.app/",
+			github_url: "http://github.com/Pjbtc/covit-19-tracker",
+			category:[ 'node.js' ]
+		},
+		{
+			name:'Realtime chat App',
+			image: chat,
+			deployed_url: "http://covid-19-tracker-by-pjbtc.web.app/",
+			github_url: "http://github.com/pjbtc/covit-19-tracker",
+			category:['mongodB']
+		},
+		{
+			name:' weather tracker',
+			image: weather,
+			deployed_url: "http://covid-19-tracker-by-pjbtc.web.app/",
+			github_url: "http://github.com/pjbtc/covit-19-tracker",
+			category:['vanilla']
+		},
+		{
+			name:'Health app',
+			image: health,
+			deployed_url: "http://covid-19-tracker-by-pjbtc.web.app/",
+			github_url: "http://github.com/pjbtc/covit-19-tracker",
+			category:['mongodB']
+		},
+		{
+			name:'Todo App',
+			image: todo,
+			deployed_url: "http://covid-19-tracker-by-pjbtc.web.app/",
+			github_url: "http://github.com/pjbtc/covit-19-tracker",
+			category:['react.js']
+		},
+]
+export default data_projects;
